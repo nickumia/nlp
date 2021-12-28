2ebb3045dab230db0a37b51e5d5d393d79154409
nickumia <nickumia6@gmail.com>
2021-12-28 03:12:55 -0500

    update: optimized ttr function, update test

--- nlp/processing/corpus/representativeness.py
+++ nlp/processing/corpus/representativeness.py
@@ -9,12 +9,4 @@ def ttr(body):
     OUT: types: int, the number of unique types
     OUT: tokens, init, the number of all tokens
     '''
-    types = {}
-
-    for word in body:
-        word = sanitize(word)
-        if word in types.keys():
-            types[word] += 1
-        else:
-            types[word] = 0
-    return len(types.keys()), len(body)
+    return len(set(body)), len(body)
