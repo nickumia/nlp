1d2528bbd3e0a44798a69e8a3eefbcb353b82635
nickumia <nickumia6@gmail.com>
2021-12-28 18:20:12 -0500

    new: add occurrences metric

--- tests/processing/test_basic_text.py
+++ tests/processing/test_basic_text.py
@@ -1,9 +1,18 @@
 
 from nlp.processing.inputs import BasicText
+from nlp.processing.corpus.representativeness import occurences
 
 
 def test_basictext_init():
-    text = BasicText('this is an interesting string.')
+    text = BasicText('this is an interesting string')
 
     assert text.getWords() == ['this', 'is', 'an', 'interesting',
-                               'string.']
+                               'string']
+
+
+def test_occurences():
+    text = BasicText('this string is the more interesting string')
+    computed = occurences(text.getWords())
+
+    assert computed == {'this': 1, 'string': 2, 'the': 1, 'is': 1, 'more': 1,
+                        'interesting': 1}
