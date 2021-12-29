aa281fbca321efde9d3002d08e75e41c14d823b9
nickumia <nickumia6@gmail.com>
2021-12-29 18:52:10 -0500

    update: BasicText class

--- nlp/processing/inputs.py
+++ nlp/processing/inputs.py
@@ -1,9 +1,14 @@
 
+import nlp.processing.corpus.identity as npci
+import nlp.processing.appraisal.basic_text as npab
+
 class BasicText():
 
     def __init__(self, original):
         self.text = original
         self.words = original.split(" ")
+        self.groups = npci.group(original)
+        self.contexts = npab.generate(self.groups, original)
 
     def getWords(self):
         return self.words
