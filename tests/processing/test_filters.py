9759765b7d981f80b19021b06eb246cf5b582cbb
nickumia <nickumia6@gmail.com>
2021-12-31 01:31:12 -0500

    new: support functions with some tests

---
+++ tests/processing/test_filters.py
@@ -0,0 +1,10 @@
+
+import nlp.processing.filters as npf
+
+
+def test_lowercase():
+    assert npf.lowercase("SUPER Important!") == "super important!"
+
+
+def test_sortabasedonb():
+    assert npf.sortabasedonb([[1,2,4,3], [3,4,2,1]]) == ([1,2,3,4], [3,4,1,2])
