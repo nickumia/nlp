0d8a5375eea3f9d18f18e13205ea134bd0e2ff12
nickumia <nickumia6@gmail.com>
2021-12-31 20:43:56 -0500

    test: add test for combinelikevalues

--- tests/processing/test_filters.py
+++ tests/processing/test_filters.py
@@ -9,3 +9,10 @@ def test_lowercase():
 def test_sortabasedonb():
     assert npf.sortabasedonb([[1, 2, 4, 3], [3, 4, 2, 1]]) == \
         ([1, 2, 3, 4], [3, 4, 1, 2])
+
+
+def test_combinelikevalues():
+    a = [1, 1, 1, 2, 3, 4]
+    b = ['a', 'a', 'a', 'b', 'c', 'd']
+    assert npf.combineLikeValues([a, b]) == ([3, 2, 3, 4],
+                                             ['a', 'b', 'c', 'd'])
