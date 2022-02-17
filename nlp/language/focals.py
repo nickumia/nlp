181918b5006f8f4bca5c03a31930d45f78ed98b0
nickumia <nickumia6@gmail.com>
2022-02-17 00:01:58 -0500

    new: priority, ranking, singular
    Higher priority is defined by closer word associations; Higher ranking is defined by the order of the senses per word (i.e. the first definition is ranked higher); Sinular is defined by the function of priority and ranking (exponential decay of priority scaled by the ranking)

--- nlp/language/focals.py
+++ nlp/language/focals.py
@@ -1,6 +1,8 @@
 
 import nlp.processing.appraisal.dictionary as npad
 import nlp.processing.corpus.identity as npci
+import nlp.processing.corpus.representativeness as npcr
+import nlp.processing.filters as npf
 import nlp.processing.storage as nps
 import nlp.language.fuzzy as nlf
 import nlp.language.constants as nlc
@@ -14,9 +16,17 @@ class WordMap(nps.Storage):
         super().__init__()
         self.focal = focal
         self.map = {0: focal}
+        self.ranking = {}
+        self.singular = {}
 
     def generateByDefinitions(self, layers=2):
+        '''
+        layers, int: the number of recursive word associations to create
+        map, dict{layer, words}: priority-ranked word associations
+            (0 -> highest priority; n -> lowest priority
+        '''
 
+        ranking = {}
         for layer in range(layers):
             all_words = []
             for word in self.map[layer]:
@@ -27,8 +37,34 @@ class WordMap(nps.Storage):
                     pos = nlf.posTag(temp_words, whole=True)
                     for item in pos:
                         if item[1] in nlc.CONTENT_WORDS:
-                            all_words += [item[0]]
-            self.map[layer+1] = all_words
+                            filtered = npf.pre_sanitize(item[0])
+                            all_words += [filtered]
+                            if filtered in ranking:
+                                ranking[filtered] += [key + 1]
+                            else:
+                                ranking[filtered] = [key + 1]
+
+            word_freq = npcr.occurences(all_words)
+            self.map[layer+1] = word_freq
+        self.ranking = ranking
+
+    def collapse(self):
+        '''
+        Calculate a weight associated with each word given the map
+        '''
+        for priority in self.map:
+            if priority == 0:
+                for word in self.map[priority]:
+                    self.singular[word] = 10
+            else:
+                for word in self.map[priority].keys():
+                    rank = sum(self.ranking[word])/float(len(self.ranking[word]))
+                    if word not in self.singular:
+                        self.singular[word] = (1 / (rank * priority)) * self.map[priority][word]
+                    else:
+                        self.singular[word] += (1 / (rank * priority)) * self.map[priority][word]
+        print(self.singular['thing'])
+        #print(self.ranking)
 
     def backup(self, filename):
         self.save = self.map
--- tests/language/test_focal.py
+++ tests/language/test_focal.py
@@ -38,12 +38,34 @@ def test_wordmap_basic():
     assert len(pam[1]) < len(pam[2])
 
 
+def test_wordmap_weights():
+    try:
+        npad.DICTIONARY.restore('dictionary.dump')
+    except FileNotFoundError:
+        # npad.DICTIONARY.prepopulate(text.words)
+        pass
+
+    A = nlfo.WordMap(['go'])
+    A.generateByDefinitions(layers=2)
+    A.backup('wordmap.dump')
+    npad.DICTIONARY.backup('dictionary.dump')
+
+    pam = A.getMap()
+    A.collapse()
+    # print(pam)
+
+    # TODO: add assertions
+
+
 if __name__ == '__main__':
     print('1 - test_wordmap_init')
     print('2 - test_wordmap_basic')
+    print('3 - test_wordmap_weights')
     a = input('Which test to run?')
 
     if a == '1':
         test_wordmap_init()
     if a == '2':
         test_wordmap_basic()
+    if a == '3':
+        test_wordmap_weights()
